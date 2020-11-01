dd1f925249cf744089947e96639dccb4c280ef4d
upinall <71399481+upinall@users.noreply.github.com>
2020-10-31 21:35:12 -0400

    Create 7-9 PhoneNumberFormat.java

---
+++ 7-9 PhoneNumberFormat.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class PhoneNumberFormat {
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner inputDevice = new Scanner(System.in);
+        do {
+            System.out.print("\nEnter the Phone Number: ");
+            String phoneText = inputDevice.nextLine();
+            if (phoneText.contains("999"))
+            break;
+
+            if (phoneText.matches("[0-9]+") && phoneText.length() == 10){
+                System.out.println("phoneNumber: " + "(" + phoneText.substring(0, 3) + ") " + phoneText.substring(3, 6) + "-" + phoneText.substring(6, 10));
+            } else {
+                System.out.println("Error, enter valid phone number");
+            }
+        } while (true);
+        inputDevice.close();
+    }
+}
