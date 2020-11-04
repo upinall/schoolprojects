4b0fab9d05da6c208c46aaf7caabea5f07df5452
upinall <71399481+upinall@users.noreply.github.com>
2020-11-04 01:45:08 -0500

    Create 8-3B CarCareChoice2.java

---
+++ 8-3B CarCareChoice2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class CarCareChoice2
+{
+   // Modify the code below
+   public static void main (String[] args)
+   {
+      Scanner input = new Scanner(System.in);
+      boolean isMatch = false;
+      String[] items =  { "oil change", "tire rotation",
+         "battery check", "brake inspection"};
+      int[] prices = {25, 22, 15, 5};
+      int x;
+      int matchIndex = 0;
+      String menu = "Enter selection:";
+      for(x = 0; x < items.length; ++x)
+        menu += "\n   " + items[x];
+      System.out.println(menu);
+      String selection = input.nextLine();
+      for (x = 0; x < items.length; x++)
+      if(selection.substring(0,3).equals(items[x].substring(0,3))) //This part is where it only takes 3 values
+      {
+	  isMatch = true;
+	  matchIndex = x;
+      }
+      if(isMatch)
+          System.out.println(selection + " price is $" + prices[matchIndex]);
+      else
+          System.out.println("Invalid Entry");
+  }
+}
