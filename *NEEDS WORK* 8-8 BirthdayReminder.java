0e7cc5ecd9af07008e12f934bfbbaf439bde7f02
upinall <71399481+upinall@users.noreply.github.com>
2020-11-15 17:34:33 -0500

    Update *NEEDS WORK* 8-8 BirthdayReminder.java

--- *NEEDS WORK* 8-8 BirthdayReminder.java
+++ *NEEDS WORK* 8-8 BirthdayReminder.java
@@ -2,55 +2,48 @@ import java.util.*;
 public class BirthdayReminder {
     public static void main(String[] args) {
         // Write your code here
+        int count=0;
+        final String SENTINEL = "ZZZ";
+        String[] names = new String[10];
+        String[] birthdays = new String[10];
+        String entry;
+        int indexSearched;
         Scanner inputDevice = new Scanner(System.in);
-        final int NUM_NAMES = 10;
-        String sentinal = "ZZZ";
-        int count = 0;
-        String name = null;
-        String birthdate = null;
-        String[] names = new String[NUM_NAMES];
-        String[] birthdates = new String[NUM_NAMES];
-        System.out.println("Enter name or type 'ZZZ' to quit");
-        name = inputDevice.nextLine();
-        while(!name.equals(sentinal) && count < 10){
-            System.out.println("Enter month and day, (e.g.mm/dd, 04/27, etc.");
-            birthdate = inputDevice.nextLine();
-            //Need a way to prevent mistakes of people forgetting 'ZZZ'
-            names[count] = name;
-            birthdates[count] = birthdate;
-            System.out.println("Enter name or type 'ZZZ' to quit");
-            name = inputDevice.nextLine();
+        System.out.println("Enter a name: ");
+        entry = inputDevice.nextLine();
+        while(!entry.equals(SENTINEL) && count<10)
+        {
+            names[count] = entry;
+            System.out.println("Enter the birthday of the " + names[count]);
+            birthdays[count] = inputDevice.nextLine();
             count++;
+            if(count == 10)
+            break;
+            System.out.println("Enter a name  or " + SENTINEL + " to stop.");
+            entry = inputDevice.nextLine();
         }
-        System.out.println("\nCount of names is " + count);
-        System.out.println("\nNames are:" + count);
-        for(int i = 0;i < count; i++)
+        System.out.println(count + " names where entered. \n");
+        for(int i = 0; i < count; ++i)
+        System.out.print(names[i] + " ");
+        System.out.println("Enter the name to be searched for: ");
+        entry = inputDevice.nextLine();
+        while(!entry.equals(SENTINEL))
         {
-            System.out.println(names[i]);
-        }
-        boolean repeat = true;
-        boolean found = false;
-        while(repeat)
-        {
-            System.out.println("Enter name or type 'ZZZ' to quit");
-            name = inputDevice.nextLine();
-            if(name.equals(sentinal))
-            repeat = false;
-            else {
-                for(int i = 0; i < count && !found; i++){
-                    if(names[i].equals(name)){
-                        found = true;
-                        birthdate = birthdates[i];
-                    }
-                }
-                if(found){
-                    System.out.println("Birthdate of " + name + " is " + birthdate);
-                }
-                else {
-                    System.out.println("Sorry, no entry for " + name);
-                }
+            indexSearched = -1; 
+        	for(int x = 0; x < count; ++x)
+            {
+            if(entry.equals(names[x])){
+                indexSearched = x;
+                break;
             }
-            found = false;
+        }
+        if(indexSearched != -1) {
+            System.out.println("Birthday of " + entry + " is " + birthdays[indexSearched]);
+        } else {
+            System.out.println("Sorry, no entry for " + entry);
+        }
+        System.out.println("Enter ANOTHER name you are looking for the birthday of or enter 'ZZZ' to stop");
+            entry = inputDevice.nextLine();
         }
     }
 }
