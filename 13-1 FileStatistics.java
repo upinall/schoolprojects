7a0201f9c56166b60f2d3ccecf7108dbc5ecbb25
upinall <71399481+upinall@users.noreply.github.com>
2020-11-18 01:23:02 -0500

    Create 13-1 FileStatistics.java

---
+++ 13-1 FileStatistics.java
@@ -0,0 +1,23 @@
+import java.nio.file.*;
+import java.nio.file.attribute.*;
+import java.io.IOException;
+public class FileStatistics {
+    public static void main(String[] args) {
+        Path file = Paths.get("/root/sandbox/FileStatistics.java");
+        // Write your code here
+        try {
+            int count = file.getNameCount();
+            System.out.println("Path is " + file.toString());
+            System.out.println("File name is " + file.getFileName());
+            System.out.println("Folder name is " + file.getName(count - 2));
+            BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+            System.out.println("File's size is " + attr.size());
+            FileTime time = attr.creationTime();
+            System.out.println("File's creation time is " + time);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Message: " + e);
+        }
+    }
+}
